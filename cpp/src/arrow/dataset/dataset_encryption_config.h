adf20a896081419cea8a2b94f08a95017b25110e
Donald R <dtolley@Donalds-Laptop.local>
2023-01-25 11:42:33 -0500

    began work in file_parquet.h to start adding per-file encryption if DataDecryptionConfiguration is not NULL

--- cpp/src/arrow/dataset/dataset_encryption_config.h
+++ cpp/src/arrow/dataset/dataset_encryption_config.h
@@ -36,5 +36,16 @@ struct PARQUET_EXPORT DatasetEncryptionConfiguration {
  
 };
 
+struct PARQUET_EXPORT DatasetDecryptionConfiguration {
+    /// core class, that translates the parameters of high level encryption 
+   std::shared_ptr<::parquet::encryption::CryptoFactory> crypto_factory;
+
+   std::shared_ptr<::parquet::encryption::KmsConnectionConfig> kms_connection_config;
+
+   std::shared_ptr<::parquet::encryption::DecryptionConfiguration> decryption_config;
+
+};
+
+
 } // namespace dataset
 } // namespace arrow    
