ac7a9e9d22099b69e9953fa96dce50e030b060b0
Donald Tolley <tolleybot@gmail.com>
2024-01-17 10:19:41 -0500

    Update cpp/src/parquet/encryption/internal_file_decryptor.cc
    Co-authored-by: Gang Wu <ustcwg@gmail.com>

--- cpp/src/parquet/encryption/internal_file_decryptor.cc
+++ cpp/src/parquet/encryption/internal_file_decryptor.cc
@@ -212,13 +212,8 @@ std::shared_ptr<Decryptor> InternalFileDecryptor::GetColumnDecryptor(
   auto aes_decryptor =
       encryption::AesDecryptor::Make(algorithm_, key_len, metadata, &all_decryptors_);
 
-  column_metadata_map_[column_path] = std::make_shared<Decryptor>(
-      aes_metadata_decryptor, column_key, file_aad_, aad, pool_);
-  column_data_map_[column_path] =
-      std::make_shared<Decryptor>(aes_data_decryptor, column_key, file_aad_, aad, pool_);
-
-  if (metadata) return column_metadata_map_[column_path];
-  return column_data_map_[column_path];
+  return std::make_shared<Decryptor>(std::move(aes_decryptor), column_key, file_aad_, aad,
+                                     pool_);
 }
 
 namespace {
